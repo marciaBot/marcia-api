eaf79a1442586cb5976cd4e4e5498ab8029b1795
Renan <renanfigcarneiro@gmail.com>
2021-03-20 00:47:33 -0300

    feat: criada interface de banco de dados do cliente

---
+++ src/main/java/com/marciaApi/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.marciaApi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.marciaApi.model.Cliente;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+	Cliente findByCpf(String cpf);
+}
