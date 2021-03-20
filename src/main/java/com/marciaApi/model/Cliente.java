f01b6595858b854eac6a95b0cdbb00e4e296a533
Renan <renanfigcarneiro@gmail.com>
2021-03-20 00:46:54 -0300

    feat: criado model do cliente

--- src/main/java/com/marciaApi/model/Cliente.java
+++ src/main/java/com/marciaApi/model/Cliente.java
@@ -1,5 +1,76 @@
 package com.marciaApi.model;
 
-public class Cliente {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+
+
+
+@Entity
+@Table(name = "CLIENTE", uniqueConstraints = {@UniqueConstraint(columnNames = {"cpf"})})
+public class Cliente {
+	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column
+	private String nome;
+	@Column(unique = true)
+	@NotNull(message = "CPF Não pode ser Nulo")
+	private String cpf;
+	@Column
+	private String numero;
+	@Column
+	private LocalDateTime createdAt = LocalDateTime.now();
+	@Column
+	private LocalDate dataNascimento;
+	
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+	
+	
+	
 }
