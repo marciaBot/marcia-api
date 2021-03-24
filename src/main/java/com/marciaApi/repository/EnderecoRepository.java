9583a894c89e2bee8a58a642d2dba9f73e1ab1d3
Renan <renanfigcarneiro@gmail.com>
2021-03-24 20:06:36 -0300

    feat: criado interface de endereco

--- src/main/java/com/marciaApi/repository/EnderecoRepository.java
+++ src/main/java/com/marciaApi/repository/EnderecoRepository.java
@@ -1,9 +1,15 @@
 package com.marciaApi.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.marciaApi.model.Cliente;
 import com.marciaApi.model.Endereco;
 
 public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+	
+	List<Endereco> findByCliente(Optional<Cliente> cliente);
 
 }
