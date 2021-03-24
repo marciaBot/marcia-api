7ee62ae24c7fad201e6c25f656646ca26633c315
Renan <renanfigcarneiro@gmail.com>
2021-03-24 00:03:43 -0300

    feat: criado service de abstracao dos metodos de cliente

---
+++ src/main/java/com/marciaApi/service/ClienteService.java
@@ -0,0 +1,46 @@
+package com.marciaApi.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marciaApi.model.Cliente;
+import com.marciaApi.repository.ClienteRepository;
+
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	ClienteRepository clienteRepository;
+	
+	public Cliente create(Cliente cliente) {
+		clienteRepository.save(cliente);
+		return cliente;
+	}
+	
+	public Cliente edit(Long id, Cliente cliente) throws Exception {
+		Optional<Cliente> clienteBase = clienteRepository.findById(id);
+		if(!clienteBase.isPresent()) {
+			throw new Exception();
+		}
+		cliente.setId(id);
+		clienteRepository.save(cliente);
+		return cliente;
+	}
+	
+	public Cliente findByCpf (String cpf) {
+		Cliente cliente = clienteRepository.findByCpf(cpf);
+		return cliente;
+	}
+	
+	public void delete (Long id) throws Exception{
+		Optional<Cliente> cliente = clienteRepository.findById(id);
+		if(!cliente.isPresent()) {
+			throw new Exception();
+		}
+		clienteRepository.deleteById(id);
+	}
+
+}
