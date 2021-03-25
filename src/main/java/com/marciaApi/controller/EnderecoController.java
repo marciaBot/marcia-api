03e378f4fe83647684d588bc82f322053a2b1ab3
Renan <renanfigcarneiro@gmail.com>
2021-03-24 21:40:18 -0300

    chore: adicionado endpoints de endereco

--- src/main/java/com/marciaApi/controller/EnderecoController.java
+++ src/main/java/com/marciaApi/controller/EnderecoController.java
@@ -1,10 +1,16 @@
 package com.marciaApi.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +35,47 @@ public class EnderecoController {
 			return ResponseEntity.ok(EnderecoDto.converter(endereco));
 		} catch (Exception e) {
 			throw new ApiRequestException("Campo nulo Inválido" , e.getCause());
+		}
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> listaEndereco(@PathVariable Long id){
+		try {
+			Endereco endereco = enderecoService.listaEndereco(id);
+			return ResponseEntity.ok(EnderecoDto.converter(endereco));
+		} catch (Exception e) {
+			throw new ApiRequestException("Campo Inválido" , e.getCause());
+		}
+	}
+	
+	@GetMapping("/listaTodos/{clienteId}")
+	public ResponseEntity<?> listaTodosEnderecos(@PathVariable Long clienteId){
+		try {
+			List<Endereco> enderecos = enderecoService.listaTodosEnderecos(clienteId);
+			return ResponseEntity.ok(enderecos.stream().map(endereco -> EnderecoDto.converter(endereco)));
+		} catch (Exception e) {
+			throw new ApiRequestException("erro", e.getCause());
+		}
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deletaEndereco(@PathVariable Long id){
+		try {
+			enderecoService.deletaEndereco(id);
+			
+			return ResponseEntity.ok().build();
+		} catch (Exception e) {
+			throw new ApiRequestException("Error", e.getCause());
+		}
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> editaEndereco(@PathVariable Long id, @RequestBody Endereco endereco){
+		try {
+			Endereco enderecoEdit = enderecoService.edit(id, endereco);
+			return ResponseEntity.ok(EnderecoDto.converter(enderecoEdit));
+		} catch (Exception e) {
+			throw new ApiRequestException("Error", e.getCause());
 		}
 	}
 }
