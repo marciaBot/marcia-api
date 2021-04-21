00bd33ea8536c210f46e0199b67f78d585210ed6
Renan <renanfigcarneiro@gmail.com>
2021-04-20 23:21:27 -0300

    feat: criação de interface de repositorio de venda

---
+++ src/main/java/com/marciaApi/repository/VendaRepository.java
@@ -0,0 +1,9 @@
+package com.marciaApi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.marciaApi.model.Venda;
+
+public interface VendaRepository extends JpaRepository<Venda, Long>{
+
+}
