e70aef8ef5f44d284cd4132ecb541d5f8e61b3b7
Renan <renanfigcarneiro@gmail.com>
2021-04-20 23:21:59 -0300

    feat: criação de service de produto

---
+++ src/main/java/com/marciaApi/service/ProdutoService.java
@@ -0,0 +1,18 @@
+package com.marciaApi.service;
+
+import com.marciaApi.model.Produto;
+import com.marciaApi.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public Produto buscarPorCodigo (String codigoBarra) {
+        Produto produto = produtoRepository.findByCodigoBarra(codigoBarra);
+        return produto;
+    }
+}
