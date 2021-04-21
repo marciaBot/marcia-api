c9e3b3b64c1bef3bdfef41132bfdca9dd697be97
Renan <renanfigcarneiro@gmail.com>
2021-04-20 23:21:10 -0300

    feat: criação de interface de repositorio de produto

---
+++ src/main/java/com/marciaApi/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.marciaApi.repository;
+
+import com.marciaApi.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+    Produto findByCodigoBarra(String codigoBarra);
+}
